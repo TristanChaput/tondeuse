c59c7f539479b59ae67d05c1f8fadeeccfbb25a0
Tristan Chaput <tristan.chaput@arolla.fr>
2023-04-10 16:27:53 +0200

    update test for direction

--- src/test/java/org/example/TondeuseTest.java
+++ src/test/java/org/example/TondeuseTest.java
@@ -1,26 +1,25 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TondeuseTest {
 
-    @Test
-    void test_show_position_0_0_N(){
-        Tondeuse tondeuse = new Tondeuse(0, 0, "N");
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0, N, 0 0 N",
+            "1, 2, E, 1 2 E",
+            "2, 1, W, 2 1 W",
+            "3, 3, S, 3 3 S"
+    })
+    void test_show_position_x_y_direction(int x, int y, String direction, String expected){
+        Tondeuse tondeuse = new Tondeuse(x, y, direction);
 
         String actual = tondeuse.showPosition();
 
-        assertThat(actual).isEqualTo("0 0 N");
-    }
-
-    @Test
-    void test_show_position_1_1_S(){
-        Tondeuse tondeuse = new Tondeuse(1, 1, "S");
-
-        String actual = tondeuse.showPosition();
-
-        assertThat(actual).isEqualTo("1 1 S");
+        assertThat(actual).isEqualTo(expected);
     }
 }
